e424d6320205eb61e2df01ed1eda229192b3de3b
sohyunj67 <sohyunj67@gmail.com>
2024-01-03 14:16:34 +0900

    spring boot 2

---
+++ merong/src/main/java/com/e7e/merong/MerongApplication.java
@@ -0,0 +1,13 @@
+package com.e7e.merong;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MerongApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MerongApplication.class, args);
+	}
+
+}
---
+++ merong/src/test/java/com/e7e/merong/MerongApplicationTests.java
@@ -0,0 +1,13 @@
+package com.e7e.merong;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MerongApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
